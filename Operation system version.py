393a0f6f5280a9f21f2d0ea6723d3751d5da44da
Rashidul-islam-Rashed <92414937+Rashidul-islam-Rashed@users.noreply.github.com>
2021-10-13 08:28:08 +0600

    Add files via upload

---
+++ Operation system version.py
@@ -0,0 +1,6 @@
+import platform
+import os
+
+print("Name of the operating system:", os.name)
+print("Name of the OS system:", platform.system())
+print("Version of the operating system:", platform.release())
